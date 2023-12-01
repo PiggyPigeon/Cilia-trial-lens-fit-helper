bb23915a18f172c27e654c3b9c52b33202050490
Bethany "Pigeon" Ellington <bethanygellington@gmail.com>
2023-12-01 15:26:00 +0800

    Initial committt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [simK, setSimK] = useState('');
+  const [bfk, setBfk] = useState('');
+
+  const handleInput = (event) => {
+    let value = event.target.value.replace(/\D/g, ''); // Remove non-numeric characters
+    if (value.length > 2) {
+      value = value.slice(0, 2) + '.' + value.slice(2);
+    }
+    setSimK(value);
+  };
+
+  const calculate = () => {
+    const value = parseFloat(simK);
+    const result = Math.round((6.293 + 0.828 * value) * 4) / 4;
+    setBfk(result.toFixed(2));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Cilia 视光小助手</h1>
+          <div>
+            <label htmlFor="simK">Sim K:</label>
+            <input
+                id="simK"
+                value={simK}
+                onChange={handleInput}
+                onKeyPress={(event) => event.key === 'Enter' && calculate()}
+            />
+            <button onClick={calculate}>计算</button>
+          </div>
+          <div>
+            推荐 BFK: {bfk}
+          </div>
+        </header>
+      </div>
   );
 }
 
